159cb25dba177b1b9c4549cc38173266598dc24d
mva13 <markvallis1987@Gmail.com>
2021-04-21 22:32:36 +0100

    Adding alias for jenkins to use

--- project/Aliases.scala
+++ project/Aliases.scala
@@ -2,6 +2,7 @@ import sbt.{Def, _}
 
 object Aliases {
   val customAliases: Seq[Def.Setting[State => State]] = {
-    addCommandAlias("runAllTests", "dockerComposeUp; test; dockerComposeStop")
+    addCommandAlias("runAllTests", "dockerComposeUp; test; dockerComposeStop") ++
+      addCommandAlias("redeployApp", "dockerComposeStop; dockerComposeUp")
   }
 }
